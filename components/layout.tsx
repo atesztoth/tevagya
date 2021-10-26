5b253f77bd551ebf57e9343888664c62c240b767
Attila Tóth <muchcoffe@gmail.com>
2021-10-26 12:23:23 +0200

    fixed screen jumping bug on mobile

--- components/layout.tsx
+++ components/layout.tsx
@@ -8,7 +8,7 @@ type Props = {
   home?: boolean
 }
 export default function Layout(props: Props) {
-  const { home, children } = props
+  const { children } = props
 
   return (
     <div className={ styles.container }>
@@ -17,12 +17,6 @@ export default function Layout(props: Props) {
         <meta
           name="description"
           content="Learn how to build a personal website using Next.js"
-        />
-        <meta
-          property="og:image"
-          content={ `https://og-image.vercel.app/${ encodeURI(
-            siteTitle
-          ) }.png?theme=light&md=0&fontSize=75px&images=https%3A%2F%2Fassets.zeit.co%2Fimage%2Fupload%2Ffront%2Fassets%2Fdesign%2Fnextjs-black-logo.svg` }
         />
         <meta name="og:title" content={ siteTitle }/>
         <meta name="twitter:card" content="summary_large_image"/>
